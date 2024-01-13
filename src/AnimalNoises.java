d904262256e9aea77c56fa140540ea584cb6cf11
AnitaWood-DMACC <118312973+arwood2@users.noreply.github.com>
2024-01-13 14:54:12 -0500

    Update AnimalNoises.java

--- src/AnimalNoises.java
+++ src/AnimalNoises.java
@@ -1,11 +1,15 @@
-
+// This Project is used for CIT175 Student - Git Activity
 
 public class AnimalNoises {
 	
-	// add your animal class to the model package
+	// add your animal class to the src folder
 	// only push this file and the animal class you created
 
 	public static void main(String[] args) {
+		
+	//
+	// Add your object Instantiations below this Line
+	//
 		Cat garfield = new Cat();
 		System.out.println(garfield.speak());
 		
